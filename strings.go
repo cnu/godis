4f9c06bd40bf574701a4ac3a6a35ce37d6c2e513
naren <makernaren@gmail.com>
2015-09-05 17:00:01 +0530

    Add INCR, DECR, INCRBY, DECRBY

--- strings.go
+++ strings.go
@@ -10,3 +10,51 @@ func (g Godis) Set(key string, value interface{}) string {
 func (g Godis) Get(key string) interface{} {
 	return g.db[key]
 }
+
+// INCR increments the key by one
+func (g Godis) INCR(key string, n int) interface{} {
+	if g.Exists(key) == 1 {
+		val := g.db[key].(int)
+		g.db[key] = val + n
+		return g.Get(key)
+	} else {
+		g.Set(key, n)
+		return g.Get(key)
+	}
+}
+
+// DECR decrements the key by one
+func (g Godis) DECR(key string, n int) interface{} {
+	if g.Exists(key) == 1 {
+		val := g.db[key].(int)
+		g.db[key] = val - n
+		return g.Get(key)
+	} else {
+		g.Set(key, n)
+		return g.Get(key)
+	}
+}
+
+// INCRBY increments the key by given value
+func (g Godis) INCRBY(key string, n int) interface{} {
+	if g.Exists(key) == 1 {
+		val := g.db[key].(int)
+		g.db[key] = val + n
+		return g.Get(key)
+	} else {
+		g.Set(key, n)
+		return g.Get(key)
+	}
+}
+
+// DECRBY decrements the key by given value
+func (g Godis) DECRBY(key string, n int) interface{} {
+	if g.Exists(key) == 1 {
+		val := g.db[key].(int)
+		g.db[key] = val - n
+		return g.Get(key)
+	} else {
+		g.Set(key, n)
+		return g.Get(key)
+	}
+}
