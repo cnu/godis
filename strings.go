0364b8898fc01fbb4601a37bae32cad472f21d28
Srinivasan R <srinivasanr@gmail.com>
2015-09-13 01:39:44 +0530

    Code and test cases for MGET

--- strings.go
+++ strings.go
@@ -10,3 +10,14 @@ func (g Godis) SET(key string, value interface{}) string {
 func (g Godis) GET(key string) interface{} {
 	return g.db[key]
 }
+
+// MGET returns a slice of values for a input slice of keys
+func (g Godis) MGET(keys ...string) []interface{} {
+	var output []interface{}
+	for _, key := range keys {
+		value := g.GET(key)
+		output = append(output, value)
+	}
+	return output
+}
+
