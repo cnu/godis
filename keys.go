81b2c3f2d3071d91270c3aeed476203fd3fdabd5
naren <makernaren@gmail.com>
2015-09-18 00:10:34 +0530

    Add RENAMENX
    Renames key to newkey if newkey does not yet exist,
    returns an error under the same conditions as RENAME.

--- keys.go
+++ keys.go
@@ -20,14 +20,26 @@ func (g Godis) DEL(keys ...string) int {
 	return count
 }
 
-// RENAME renames a key to newkey. Returns an error when the source
-// and destination names are the same, or when key does not exist. If new key
+// RENAME renames a key to newkey. Returns an error when the key
+// and newkey are the same, or when key does not exist. If new key
 // already exists it is overwritten.
 func (g Godis) RENAME(key, newKey string) interface{} {
 	if key != newKey || g.EXISTS(key) != 0 {
 		if g.EXISTS(newKey) > 0 {
 			g.DEL(newKey)
 		}
+		val := g.GET(key)
+		g.DEL(key)
+		return g.SET(newKey, val)
+	} else {
+		return false
+	}
+}
+
+// Renames key to newkey if newkey does not yet exist.
+// Returns an error under the same conditions as RENAME.
+func (g Godis) RENAMENX(key, newKey string) interface{} {
+	if (key != newKey || g.EXISTS(key) != 0) && g.EXISTS(newKey) == 0 {
 		val := g.GET(key)
 		g.DEL(key)
 		return g.SET(newKey, val)
