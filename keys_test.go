ed3d18f7d4f980b1543e2a118f4fc9f908c5f61f
naren <makernaren@gmail.com>
2015-10-25 23:21:31 +0530

    Use GET to test value of RENAMEd key

--- keys_test.go
+++ keys_test.go
@@ -90,10 +90,10 @@ func TestRENAME(t *testing.T) {
 	key := "myKey"
 	newKey := "hisKey"
 	db.SET(key, "value")
-	got, err := db.RENAME(key, newKey)
-	if err != nil {
-		t.Errorf("RENAME(%q, %q) == %q, %v want %q, <nil>", key, newKey, got,
-			err, newKey)
+	db.RENAME(key, newKey)
+	got, err := db.GET(newKey)
+	if got != "value" || err != nil {
+		t.Errorf("GET(%q) == %q, %v want %q, <nil>", newKey, got, err, "value")
 	}
 }
 
