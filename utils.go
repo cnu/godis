7e50aa160a7ea86acc06253c5ac657ad1bc5759c
naren <makernaren@gmail.com>
2015-11-09 21:32:10 +0530

    Add getAllKeys method
    Internal method used to get all keys from the entire DB.
    TODO : Lock the whole db while getting iterating the keys
    for consistant results?

--- utils.go
+++ utils.go
@@ -27,3 +27,18 @@ func (g *Godis) destroyInMillis(key string, exp uint64) (int, error) {
 	time.Sleep(time.Duration(exp) * time.Millisecond)
 	return g.DEL(key)
 }
+
+// Get all the keys in the entire database
+// TODO : Lock the whole db while getting its keys for consistant results?
+func (g *Godis) getAllKeys() ([]string, error) {
+	db := g.db
+	keys := make([]string, len(db))
+	if len(db) > 0 {
+		i := 0
+		for k := range db {
+			keys[i] = k //not using append as we know exact length of the db
+			i++
+		}
+	}
+	return keys, nil
+}
